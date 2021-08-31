9a6b62ff4ac640d1de0adf5dd9d121ad08d148df
Leandro Costa <leandro.costa@npu.com.br>
2021-08-30 23:05:25 -0400

    Adicionado o login

--- public/routes.php
+++ public/routes.php
@@ -6,7 +6,11 @@ use Pecee\SimpleRouter\Exceptions\NotFoundHttpException;
 use Pecee\SimpleRouter\SimpleRouter;
 
 SimpleRouter::get('/cadastrar', [User::class, 'cadastrar']);
+SimpleRouter::get('/quem-somos', [Home::class, 'quemSomos']);
+SimpleRouter::get('/login', [Home::class, 'login']);
 SimpleRouter::get('/', [Home::class, 'initialPage']);
+SimpleRouter::post('/login', [Home::class, 'logar']);
+
 
 
 try {
--- public/src/controller/Home.php
+++ public/src/controller/Home.php
@@ -23,8 +23,23 @@ class Home
         include_once("src/view/main/footer.phtml");
     }
 
+    public function quemSomos():void
+    {
+        $header = new Header("Quem Somos");
+        include_once("src/view/main/header.phtml");
+        include_once("src/view/main/menu.phtml");
+        include_once("src/view/home/quem-somos.phtml");
+        include_once("src/view/main/footer.phtml");
+    }
+
     public function ops():void
     {
         include_once("src/view/main/404.phtml");
+    }
+
+    public function logar():void
+    {
+
+        var_dump($_POST);
     }
 }
